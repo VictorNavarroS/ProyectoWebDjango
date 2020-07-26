0f5f77a1f25290ed8a17370bd782bfa4b57e90d9
VictorNavarroS <victornavarrosoto@gmail.com>
2020-07-26 16:08:40 -0400

    Configuraciones servicios -2

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -4,8 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home,name="Home"),
-    #path('servicios/',views.servicios,name="Servicios"),
+    path('',views.home,name="Home"),    
     path('tienda/',views.tienda,name="Tienda"),
     path('blog/',views.blog,name="Blog"),
     path('contacto/',views.contacto,name="Contacto"),    
--- servicios/views.py
+++ servicios/views.py
@@ -4,4 +4,4 @@ from .models import Servicio
 # Create your views here.
 def servicios(request):
     servis=Servicio.objects.all()
-    return render(request,"ProyectoWebApp/servicios.html",{"servicios":servis})
+    return render(request,"servicios/servicios.html",{"servicios":servis})
